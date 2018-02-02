98263abe2ec71816b3dff13e1cab6acaaade339e
david.benabarre <david.benabarre@est.fib.upc.edu>
2018-02-02 16:13:59 +0100

    initial commit

---
+++ Node.cpp
@@ -0,0 +1,48 @@
+//
+// Created by dabi on 02/02/18.
+//
+
+#include "Node.h"
+
+int Node::getNumOfChildren() const {
+    return node.numOfChildren;
+}
+
+void Node::setNumOfChildren(int numOfChildren) {
+    node.numOfChildren = numOfChildren;
+}
+
+int Node::getVal() const {
+    return node.val;
+}
+
+void Node::setVal(int val) {
+    node.val = val;
+}
+
+int Node::getBranchVal() const {
+    return node.branchVal;
+}
+
+void Node::setBranchVal(int branchVal) {
+    node.branchVal = branchVal;
+}
+
+int Node::getAttribute() const {
+    return node.attribute;
+}
+
+void Node::setAttribute(int attribute) {
+    node.attribute = attribute;
+}
+
+Node *const *Node::getChild() const {
+    return node.child;
+}
+
+void Node::setChild(Node child, int i) {
+    node.child[i] = &child;
+}
+Node::Node(const Node::innerNode &node) : node(node) {}
+
+Node::Node() {}
---
+++ Node.h
@@ -0,0 +1,55 @@
+//
+// Created by dabi on 02/02/18.
+//
+
+#ifndef PARALLEL_DECISION_TREE_CLASSIFIER_MASTER_NODE_H
+#define PARALLEL_DECISION_TREE_CLASSIFIER_MASTER_NODE_H
+
+
+
+
+class Node {
+
+private:
+
+    struct innerNode{
+        int numOfChildren;
+        int val;
+        int branchVal;
+        int attribute;
+        struct Node *child[10];
+    };
+
+    struct innerNode node;
+
+public:
+
+    Node(const innerNode &node);
+
+    Node();
+
+    int getNumOfChildren() const;
+
+    void setNumOfChildren(int numOfChildren);
+
+    int getVal() const;
+
+    void setVal(int val);
+
+    int getBranchVal() const;
+
+    void setBranchVal(int branchVal);
+
+    int getAttribute() const;
+
+    void setAttribute(int attribute);
+
+    Node *const *getChild() const;
+
+    void setChild(Node child, int i);
+
+
+};
+
+
+#endif //PARALLEL_DECISION_TREE_CLASSIFIER_MASTER_NODE_H
